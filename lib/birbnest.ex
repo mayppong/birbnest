2db1eca528c0e0a5f8c6dff25245ede62716fffd
May Pongpitpitak <may.pognpitpitak@aggregateiq.com>
2016-09-21 13:23:40 -0700

    Make `birbnest` an OTP application.
    To easily make sure Agent process is started, we make it `birbnest` an
    OTP application that can be added to a supervision tree for other apps.
    
    This isn't really tested so I hope it works.

--- lib/birbnest.ex
+++ lib/birbnest.ex
@@ -1,2 +1,12 @@
 defmodule Birbnest do
+  use Application
+
+  def start(_type, _args) do
+    import Supervisor.Spec
+
+    children = [
+      worker(Birbnest.Nest, [])
+    ]
+    Supervisor.start_link(children, [])
+  end
 end
