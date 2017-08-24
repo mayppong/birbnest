3c3e8666edb711aef9972bb9417ca37b3186450c
May Pongpitpitak <me@mayppong.com>
2017-08-23 22:48:40 -0700

    Document @spec and clean up

--- lib/birbnest/nest.ex
+++ lib/birbnest/nest.ex
@@ -5,10 +5,12 @@ defmodule Birbnest.Nest do
 
   @behaviour Plug.Session.Store
   @name __MODULE__
+  @type on_start :: {:ok, pid} | {:error, {:already_started, pid} | term}
 
   @doc """
   Start the Agent with empty map and module name as reference id.
   """
+  @spec start_link() :: on_start
   def start_link do
     Agent.start_link(&Map.new/0, name: @name)
   end
@@ -22,10 +24,11 @@ defmodule Birbnest.Nest do
   @doc """
   Get session data from the agent for the given session id.
   """
+  @spec get(%Plug.Conn{}, String.t, Keyword.t) :: {String.t | nil, Map.t}
   def get(_conn, sid, _opts) do
-    case Agent.get(@name, &Map.get(&1, sid)) do
-      nil ->
-        {nil, %{}}
+    Agent.get(@name, &Map.get(&1, sid))
+    |> case do
+      nil -> {nil, %{}}
       data -> {sid, data}
     end
   end
@@ -34,6 +37,7 @@ defmodule Birbnest.Nest do
   Put session data into the agent for the given session id. If `nil` is given
   for the session id, create a new session.
   """
+  @spec put(%Plug.Conn{}, String.t | nil, Any.t, Keyword.t) :: String.t
   def put(conn, nil, value, opts), do: put(conn, UUID.uuid4(), value, opts)
   def put(_conn, sid, value, _opts) do
     Agent.update(@name, &Map.put(&1, sid, value))
@@ -43,6 +47,7 @@ defmodule Birbnest.Nest do
   @doc """
   Remove the given session id from store.
   """
+  @spec delete(%Plug.Conn{}, String.t, Keyword.t) :: :ok
   def delete(_conn, sid, _opts) do
     Agent.update(@name, &Map.delete(&1, sid))
     :ok
